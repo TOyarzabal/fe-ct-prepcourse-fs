ecf6b077f7c5f936b3f0f3e91a3b69e34962e41d
TOyarzabal <oyarzabal.tiago@gmail.com>
2024-05-21 21:10:06 -0300

    Arrays

--- C7 Arrays/ejercicios/03.js
+++ C7 Arrays/ejercicios/03.js
@@ -1,6 +1,8 @@
 function obtenerLargoDelArray(array) {
   // Retornar la longitud del arreglo recibido.
-  // Tu código:
+  // Tu código: 
+  if (array) {return array.length;} 
+  else {return 0;}
 }
 
 module.exports = obtenerLargoDelArray;
